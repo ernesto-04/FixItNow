db36b57ebc1ebc4032a72d7a452fa2df65f7c2be
ernesto-04 <yumingernesto@gmail.com>
2026-03-22 16:29:38 +0700

    technician list

---
+++ FixItNow.Web/TechnicianController.cs
@@ -0,0 +1,25 @@
+﻿using FixItNow.Infrastructure.Models.Commons;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FixItNow.Web
+{
+    [Route("api/technicians")]
+    [ApiController]
+    public class TechnicianController : ControllerBase
+    {
+        private readonly FixItNowDataContext _context;
+
+        public TechnicianController(FixItNowDataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetTechnicians()
+        {
+            var technicians = _context.Technicians.ToList();
+            return Ok(technicians);
+        }
+    }
+}
